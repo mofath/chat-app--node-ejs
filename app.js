21e2aa5ce65663750792ddc1daf7ad0cde22a153
mofath <mohamed.elgaedi@gmail.com>
2020-11-10 16:36:16 +0200

    Added Navbar

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ const path = require('path');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-    res.send("Hello, from Node");
-});
 
-app.use(express.static(path.join(__dirname, 'assets')));
+app.use(express.static(path.join(__dirname, "assets")));
 app.set('view engine', 'ejs');
-app.set('views', 'views'); 
+app.set('views', 'views');
+
+app.use('/', (req, res, next) => {
+    res.render("index");
+});
 
 const PORT = 5000;
 
 app.listen(PORT, () =>
     console.log(`server is listening at port ${PORT}`)
-)
+);

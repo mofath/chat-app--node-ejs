5dce0d16a96fa8ea4bc7f2ea63a21b616e596b92
mofath <mohamed.elgaedi@gmail.com>
2020-11-11 04:12:45 +0200

    Added signup page

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const DBManager = require("./lib/DBManager");
+const authRouter = require("./routes/auth.route");
 
 const app = express();
 
-
 app.use(express.static(path.join(__dirname, "assets")));
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-app.use('/', (req, res, next) => {
-    res.render("index");
-});
+/**
+ * Instatiatin db instance and connect to db
+ */
+const DBInstance = new DBManager();
+DBInstance.CONNECT();
+
+app.use("/", authRouter);
 
 const PORT = 5000;
 
-app.listen(PORT, () =>
-    console.log(`server is listening at port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`server is listening at port ${PORT}`));
